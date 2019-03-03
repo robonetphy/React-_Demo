a9ea9cb7697f3257845a4076947c7e4aa4fe9a58
UmangGPatel <1998patelumang@gmail.com>
2019-03-03 23:42:21 +0530

    Updates

---
+++ precache-manifest.244eb835a8c70d1a1989c454b33058a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "96f4c681eccf636664dc",
+    "url": "/React_Demo/static/css/main.7bb483ac.chunk.css"
+  },
+  {
+    "revision": "96f4c681eccf636664dc",
+    "url": "/React_Demo/static/js/main.96f4c681.chunk.js"
+  },
+  {
+    "revision": "4471b721bb62e4ff9af9",
+    "url": "/React_Demo/static/js/runtime~main.4471b721.js"
+  },
+  {
+    "revision": "769497c23799890ff49a",
+    "url": "/React_Demo/static/js/2.769497c2.chunk.js"
+  },
+  {
+    "revision": "2a3e7bcc624918e19eb1948b9760fb11",
+    "url": "/React_Demo/index.html"
+  }
+];
